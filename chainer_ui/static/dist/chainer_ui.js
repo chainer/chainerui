53bc92632b6ea45ce77abb684472eac1243c7e25
Masaki KOBAYASHI <makky.4d6b.3f5@gmail.com>
2017-08-23 11:03:40 +0900

    update assets

--- chainer_ui/static/dist/chainer_ui.js
+++ chainer_ui/static/dist/chainer_ui.js
@@ -1379,8 +1379,6 @@ AxisScaleSelector.defaultProps = {
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__LineConfigurator__ = __webpack_require__(907);
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
-
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
@@ -1448,9 +1446,9 @@ var LinesConfigurator = function (_React$Component) {
       var addingLine = this.state.addingLine;
 
 
-      var hasSameLine = !![].concat(_toConsumableArray(lines.filter(function (l) {
+      var hasSameLine = lines.some(function (l) {
         return l.resultId === addingLine.resultId && l.logKey === addingLine.logKey;
-      }))).length;
+      });
 
       if (addingLine.resultId == null || addingLine.logKey == null || hasSameLine) {
         // invalid or hasSameLine
