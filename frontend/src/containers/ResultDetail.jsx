07a4a3705467d42dac7f48154896dbc2b63814e5
scarletrunner7000 <scarletrunner7000@gmail.com>
2017-09-05 14:12:10 +0900

    add polling to result detail page

--- frontend/src/containers/ResultDetail.jsx
+++ frontend/src/containers/ResultDetail.jsx
@@ -3,18 +3,32 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Container } from 'reactstrap';
 import {
-  // loadResults, updateResult, deleteResult,
+  loadResults,
   updateGlobalPollingRate,
   updateGlobalChartSize
 } from '../actions';
 import NavigationBar from '../components/NavigationBar';
 import { defaultConfig } from '../constants';
+import { startPolling, stopPolling } from '../utils';
 
 class ResultDetail extends React.Component {
-  constructor(props) {
-    super(props);
+  componentDidMount() {
+    const { pollingRate } = this.props.config.global;
+    this.resultsPollingTimer = startPolling(this.props.loadResults, pollingRate);
+  }
 
-    this.state = {};
+  componentWillReceiveProps(nextProps) {
+    const currentPollingRate = this.props.config.global.pollingRate;
+    const nextPollingRate = nextProps.config.global.pollingRate;
+
+    if (currentPollingRate !== nextPollingRate) {
+      stopPolling(this.resultsPollingTimer);
+      this.resultsPollingTimer = startPolling(this.props.loadResults, nextPollingRate);
+    }
+  }
+
+  componentWillUnmount() {
+    stopPolling(this.resultsPollingTimer);
   }
 
   render() {
@@ -58,11 +72,13 @@ ResultDetail.propTypes = {
   config: PropTypes.shape({
     global: PropTypes.objectOf(PropTypes.any)
   }).isRequired,
+  loadResults: PropTypes.func.isRequired,
   updateGlobalPollingRate: PropTypes.func.isRequired,
   updateGlobalChartSize: PropTypes.func.isRequired
 };
 
 export default connect(mapStateToProps, {
+  loadResults,
   updateGlobalPollingRate,
   updateGlobalChartSize
 })(ResultDetail);
