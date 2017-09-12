7b4a5aaf140abac0f73b7b13081bf763d9800aef
Masaki KOBAYASHI <makky.4d6b.3f5@gmail.com>
2017-09-12 10:49:00 +0900

    fix migration command error

--- chainer_ui/migration/env.py
+++ chainer_ui/migration/env.py
@@ -1,52 +1,14 @@
-from __future__ import with_statement
-from alembic import context
+''' env.py '''
+
+
+import alembic
 from sqlalchemy import engine_from_config, pool
-from logging.config import fileConfig
-
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
-config = context.config
-
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
-fileConfig(config.config_file_name)
-
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-from chainer_ui.models.result import Result
-
-target_metadata = [
-    Result.metadata
-]
-
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
-
-def run_migrations_offline():
-    """Run migrations in 'offline' mode.
-
-    This configures the context with just a URL
-    and not an Engine, though an Engine is acceptable
-    here as well.  By skipping the Engine creation
-    we don't even need a DBAPI to be available.
-
-    Calls to context.execute() here emit the given string to the
-    script output.
-
-    """
-    url = config.get_main_option("sqlalchemy.url")
-    context.configure(
-        url=url, target_metadata=target_metadata, literal_binds=True)
-
-    with context.begin_transaction():
-        context.run_migrations()
 
 
-def run_migrations_online():
+from chainer_ui import SQLALCHEMY_DATABASE_URI
+
+
+def run_migrations_online(config):
     """Run migrations in 'online' mode.
 
     In this scenario we need to create an Engine
@@ -59,15 +21,17 @@ def run_migrations_online():
         poolclass=pool.NullPool)
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection,
-            target_metadata=target_metadata
-        )
+        alembic.context.configure(connection=connection)
 
-        with context.begin_transaction():
-            context.run_migrations()
+        with alembic.context.begin_transaction():
+            alembic.context.run_migrations()
 
-if context.is_offline_mode():
-    run_migrations_offline()
-else:
-    run_migrations_online()
+
+def main():
+    ''' main '''
+    config = alembic.config.Config()
+    config.set_main_option("sqlalchemy.url", SQLALCHEMY_DATABASE_URI)
+    run_migrations_online(config)
+
+
+main()
