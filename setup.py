2b681a1cba3f33f6816fc5e46cdc309a0b72217f
scarletrunner7000 <scarletrunner7000@gmail.com>
2017-09-13 11:27:05 +0900

    change version to v0.0.6

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='chainer_ui',
-    version='0.0.5',
+    version='0.0.6',
     description='chainer ui',
     long_description=readme,
     install_requires=required,
