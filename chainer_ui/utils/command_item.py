676f102f4fd90daf374b7bda57893820a40b683f
scarletrunner7000 <scarletrunner7000@gmail.com>
2017-09-14 10:57:35 +0900

    add command_item class

--- chainer_ui/utils/command_item.py
+++ chainer_ui/utils/command_item.py
@@ -11,6 +11,8 @@ from is_jsonable import is_jsonable
 
 class CommmandItem:
 
+    file_name = 'commands'
+
     def __init__(self, name=None, request=None, response=None):
         self.from_dict({
             'name': name,
@@ -71,12 +73,31 @@ class CommmandItem:
         return True
 
     @staticmethod
-    def load(path):
-        pass
+    def load_commands(result_path, file_name=self.file_name):
+        result_path = os.path.abspath(result_path)
+        commands_path = os.path.join(result_path, file_name)
+        commands = []
+
+        if os.path.isfile(commands_path):
+            with open(commands_path, 'r') as f:
+                try:
+                    commands = json.load(f)
+                except json.decoder.JSONDecodeError as e:
+                    pass
+
+        return list(map(lambda cmd: CommmandItem(**cmd), commands))
 
     @staticmethod
-    def dump(path):
-        pass
+    def dump_commands(commands, result_path, file_name=self.file_name):
+        result_path = os.path.abspath(result_path)
+        commands_path = os.path.join(result_path, file_name)
+
+        fd, path = tempfile.mkstemp(prefix=file_name, dir=result_path)
+        with os.fdopen(fd, 'w') as f:
+            json.dump(list(map(lambda cmd: cmd.to_dict(), commands)),
+                      f, indent=4)
+
+        shutil.move(path, commands_path)
 
     def from_dict(self, command_dict):
         self._name = command_dict.get('name', None)
