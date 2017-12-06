f17607f0f18306654705011637edf65a31d6634d
gky360 <gky360@gmail.com>
2017-12-06 20:09:35 +0800

    change version to v0.1.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='chainerui',
-    version='0.0.9',
+    version='0.1.0',
     description='chainerui',
     long_description=readme,
     install_requires=required,
