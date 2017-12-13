e46551bec016be2a22e2a03a6a106110ddbbe4db
TANAKA Daisuke <tanakad@preferred.jp>
2017-12-13 13:52:54 +0900

    apply flake8 to tests and root file

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
+from setuptools import find_packages
+from setuptools import setup
 
 with open('README.md', 'r', encoding='utf-8') as f:
     readme = f.read()
