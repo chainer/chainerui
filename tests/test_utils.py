6a4f2c55c2b56a6a4e9fd5f1b4ccbf12f0f5ac4f
ofk <ofkjpn+github@gmail.com>
2018-01-12 15:55:04 +0900

    Fix operator

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -9,8 +9,8 @@ class TestUtils(unittest.TestCase):
 
     def test_is_jsonable(self):
         assert is_jsonable(['string'])
-        assert is_jsonable([datetime.now()]) is False
+        assert not is_jsonable([datetime.now()])
 
     def test_is_numberable(self):
         assert is_numberable('12345')
-        assert is_numberable('abcde') is False
+        assert not is_numberable('abcde')
