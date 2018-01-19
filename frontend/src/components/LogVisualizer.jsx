4d28e09885de8e3ffd716ecb66b87a7f20898ee2
ofk <ofkjpn+github@gmail.com>
2018-01-19 19:22:11 +0900

    Rewrite to stateless function

--- frontend/src/components/LogVisualizer.jsx
+++ frontend/src/components/LogVisualizer.jsx
@@ -75,111 +75,109 @@ const buildLineElems = (
   return lineElems;
 };
 
-class LogVisualizer extends React.Component {
-  render() {
-    const {
-      results = {},
-      projectConfig = {},
-      stats
-    } = this.props;
-    const { axes, resultsConfig = {}, lines = {} } = projectConfig;
-    const { logKeys = [] } = stats;
-    const {
-      xAxis = { axisName: 'xAxis' },
-      yLeftAxis = { axisName: 'yLeftAxis' },
-      yRightAxis = { axisName: 'yRightAxis' }
-    } = axes || {};
-    const { xAxisKey = 'epoch' } = xAxis;
-    const selectedResults = getSelectedResults(results, resultsConfig);
-    const selectedLogKeys = {
-      yLeftAxis: getSelectedLogKeys(yLeftAxis.logKeysConfig),
-      yRightAxis: getSelectedLogKeys(yRightAxis.logKeysConfig)
-    };
+const LogVisualizer = (props) => {
+  const {
+    results = {},
+    projectConfig = {},
+    stats
+  } = props;
+  const { axes, resultsConfig = {}, lines = {} } = projectConfig;
+  const { logKeys = [] } = stats;
+  const {
+    xAxis = { axisName: 'xAxis' },
+    yLeftAxis = { axisName: 'yLeftAxis' },
+    yRightAxis = { axisName: 'yRightAxis' }
+  } = axes || {};
+  const { xAxisKey = 'epoch' } = xAxis;
+  const selectedResults = getSelectedResults(results, resultsConfig);
+  const selectedLogKeys = {
+    yLeftAxis: getSelectedLogKeys(yLeftAxis.logKeysConfig),
+    yRightAxis: getSelectedLogKeys(yRightAxis.logKeysConfig)
+  };
 
-    const dataDict = {}; // ex. 1: { epoch: 1, 12_main_loss: 0.011, ... }
-    ['yLeftAxis', 'yRightAxis'].forEach((axisName) => {
-      selectedResults.forEach((resultId) => {
-        const result = results[resultId];
-        if (result == null) {
-          return;
-        }
-        selectedLogKeys[axisName].forEach((logKey) => {
-          const line = lines[line2key({ resultId, logKey })] ||
-            createLine(resultId, logKey, results, logKeys);
-          const logs = result.logs || [];
-          logs.forEach((log) => {
-            const logDict = {};
-            log.logItems.forEach((logItem) => {
-              logDict[logItem.key] = logItem.value;
-            });
-            if (logDict[xAxisKey] == null || logDict[logKey] == null) {
-              return;
-            }
-            if (dataDict[logDict[xAxisKey]] == null) {
-              dataDict[logDict[xAxisKey]] = { [xAxisKey]: logDict[xAxisKey] };
-            }
-            dataDict[logDict[xAxisKey]][line2dataKey(line, axisName)] = logDict[logKey];
+  const dataDict = {}; // ex. 1: { epoch: 1, 12_main_loss: 0.011, ... }
+  ['yLeftAxis', 'yRightAxis'].forEach((axisName) => {
+    selectedResults.forEach((resultId) => {
+      const result = results[resultId];
+      if (result == null) {
+        return;
+      }
+      selectedLogKeys[axisName].forEach((logKey) => {
+        const line = lines[line2key({ resultId, logKey })] ||
+              createLine(resultId, logKey, results, logKeys);
+        const logs = result.logs || [];
+        logs.forEach((log) => {
+          const logDict = {};
+          log.logItems.forEach((logItem) => {
+            logDict[logItem.key] = logItem.value;
           });
+          if (logDict[xAxisKey] == null || logDict[logKey] == null) {
+            return;
+          }
+          if (dataDict[logDict[xAxisKey]] == null) {
+            dataDict[logDict[xAxisKey]] = { [xAxisKey]: logDict[xAxisKey] };
+          }
+          dataDict[logDict[xAxisKey]][line2dataKey(line, axisName)] = logDict[logKey];
         });
       });
     });
-    const data = Object.keys(dataDict).map((key) => (dataDict[key]));
+  });
+  const data = Object.keys(dataDict).map((key) => (dataDict[key]));
 
-    const lineElems = [
-      ...buildLineElems(selectedResults, selectedLogKeys.yLeftAxis, 'yLeftAxis', results, projectConfig, logKeys),
-      ...buildLineElems(selectedResults, selectedLogKeys.yRightAxis, 'yRightAxis', results, projectConfig, logKeys)
-    ];
+  const lineElems = [
+    ...buildLineElems(selectedResults, selectedLogKeys.yLeftAxis, 'yLeftAxis', results, projectConfig, logKeys),
+    ...buildLineElems(selectedResults, selectedLogKeys.yRightAxis, 'yRightAxis', results, projectConfig, logKeys)
+  ];
 
-    const { chartSize } = this.props.globalConfig;
+  const { chartSize } = props.globalConfig;
 
-    return (
-      <div className="log-visualizer-root">
-        <ResponsiveContainer
-          width={chartSize.width}
-          height={chartSize.height}
-          aspect={chartSize.aspect}
-        >
-          <LineChart data={data}>
-            <XAxis
-              type="number"
-              dataKey={xAxisKey}
-              scale={xAxis.scale}
-              domain={getDomain(xAxis)}
-              allowDataOverflow
-            />
-            <YAxis
-              yAxisId="yLeftAxis"
-              orientation="left"
-              scale={yLeftAxis.scale}
-              domain={getDomain(yLeftAxis)}
-              tickFormatter={formatLogValue()}
-              allowDataOverflow
-            />
-            <YAxis
-              yAxisId="yRightAxis"
-              orientation="right"
-              scale={yRightAxis.scale}
-              domain={getDomain(yRightAxis)}
-              tickFormatter={formatLogValue()}
-              allowDataOverflow
-            />
-            <CartesianGrid strokeDasharray="3 3" />
-            <Tooltip
-              content={
-                <LogVisualizerTooltip
-                  results={results}
-                  lines={lines}
-                  xAxisKey={xAxisKey}
-                />
-              }
-            />
-            {lineElems}
-          </LineChart>
-        </ResponsiveContainer>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="log-visualizer-root">
+      <ResponsiveContainer
+        width={chartSize.width}
+        height={chartSize.height}
+        aspect={chartSize.aspect}
+      >
+        <LineChart data={data}>
+          <XAxis
+            type="number"
+            dataKey={xAxisKey}
+            scale={xAxis.scale}
+            domain={getDomain(xAxis)}
+            allowDataOverflow
+          />
+          <YAxis
+            yAxisId="yLeftAxis"
+            orientation="left"
+            scale={yLeftAxis.scale}
+            domain={getDomain(yLeftAxis)}
+            tickFormatter={formatLogValue()}
+            allowDataOverflow
+          />
+          <YAxis
+            yAxisId="yRightAxis"
+            orientation="right"
+            scale={yRightAxis.scale}
+            domain={getDomain(yRightAxis)}
+            tickFormatter={formatLogValue()}
+            allowDataOverflow
+          />
+          <CartesianGrid strokeDasharray="3 3" />
+          <Tooltip
+            content={
+              <LogVisualizerTooltip
+                results={results}
+                lines={lines}
+                xAxisKey={xAxisKey}
+              />
+            }
+          />
+          {lineElems}
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
 
 LogVisualizer.propTypes = {
   results: PropTypes.objectOf(PropTypes.any).isRequired,
