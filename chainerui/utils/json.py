71bc8cf3bcde0b04cf74e7ff9da0132399faf3e5
ofk <ofkjpn+github@gmail.com>
2018-01-26 11:49:10 +0900

    Use the fastest JSON library that installed

--- chainerui/utils/json.py
+++ chainerui/utils/json.py
@@ -1,4 +1,10 @@
-from json import dump  # NOQA
-from json import dumps  # NOQA
-from json import load  # NOQA
-from json import loads  # NOQA
+try:
+    from ujson import dump  # NOQA
+    from ujson import dumps  # NOQA
+    from ujson import load  # NOQA
+    from ujson import loads  # NOQA
+except ImportError:
+    from json import dump  # NOQA
+    from json import dumps  # NOQA
+    from json import load  # NOQA
+    from json import loads  # NOQA
