c83e1523cea1cee9c06d1ca23851d57f23476d97
TANAKA Daisuke <tanakad@preferred.jp>
2018-01-30 16:30:43 +0900

    add commands test

--- tests/extensions_tests/test_commands_extension.py
+++ tests/extensions_tests/test_commands_extension.py
@@ -2,20 +2,43 @@ import os
 import shutil
 import tempfile
 import unittest
+from unittest.mock import MagicMock
 
 from chainer.training import Trainer
 from chainer.training.triggers import IntervalTrigger
 
-from chainerui.extensions.commands_extension import _CommandIntervalTrigger
+from chainerui.extensions.commands_extension import _stop_training
 from chainerui.extensions.commands_extension import CommandsExtension
+from chainerui.utils.command_item import CommandItem
 from chainerui.utils.commands_state import CommandsState
 from chainerui.utils.commands_state import JobStatus
 
 
 class _MockTrainer(Trainer):
-    def __init__(self, out_path):
+    def __init__(self, out_path, stop_trigger=IntervalTrigger(100, 'epoch'),
+                 updater=None):
         self.out = out_path
-        self.stop_trigger = IntervalTrigger(100, 'epoch')
+        self.stop_trigger = stop_trigger
+
+        hyperparam = MagicMock()
+        hyperparam.get_dict.return_value = {'lr': 0.005}
+        optimizer = MagicMock()
+        optimizer.__class__.__name__ = 'MomentumSGD'
+        optimizer.hyperparam = hyperparam
+
+        if updater is None:
+            updater = MagicMock()
+            updater.epoch = 1
+            updater.iteration = 1
+            updater.get_optimizer.return_value = optimizer
+        self.updater = updater
+
+    @property
+    def elapsed_time(self):
+        return 0
+
+    def serialize(self, serializer):
+        pass
 
 
 class TestCommandsExtension(unittest.TestCase):
@@ -28,7 +51,7 @@ class TestCommandsExtension(unittest.TestCase):
         if os.path.exists(self._dir):
             shutil.rmtree(self._dir)
 
-    def test_call_no_command(self):
+    def test_initialize_command_trigger(self):
         out_path = os.path.join(self._dir, 'results')
         os.makedirs(out_path)
         commands_path = os.path.join(out_path, 'commands')
@@ -36,11 +59,146 @@ class TestCommandsExtension(unittest.TestCase):
         assert os.path.isfile(commands_path)
 
         target = CommandsExtension()
+        trigger = MagicMock()
+        trainer = _MockTrainer(out_path, trigger)
+        target.initialize(trainer)
+        assert not trainer.stop_trigger._loop_stop
+        _stop_training(trainer, None)
+        assert trainer.stop_trigger._loop_stop
+
+    def test_call(self):
+        out_path = os.path.join(self._dir, 'results')
+        os.makedirs(out_path)
+        commands_path = os.path.join(out_path, 'commands')
+        open(commands_path, 'w').close()
+        assert os.path.isfile(commands_path)
+
+        # initialize
+        target = CommandsExtension()
         trainer = _MockTrainer(out_path)
         target.initialize(trainer)
-        assert type(trainer.stop_trigger) is _CommandIntervalTrigger
+        assert not trainer.stop_trigger._loop_stop
         assert not os.path.isfile(commands_path)
         assert CommandsState.job_status(out_path) == JobStatus.RUNNING
 
+        # setup valid command
+        commands = CommandItem.load_commands(out_path)
+        command = CommandItem(name='take_snapshot')
+        command.set_request(CommandItem.REQUEST_OPEN, None, None)
+        commands.append(command)
+        command2 = CommandItem(name='stop')
+        command2.set_request(
+            CommandItem.REQUEST_OPEN,
+            None,
+            {'key': 'epoch', 'value': 10}
+        )
+        commands.append(command2)
+        command3 = CommandItem(name='adjust_hyperparams')
+        command3.set_request(
+            CommandItem.REQUEST_OPEN,
+            {'optimizer': 'MomentumSGD', 'hyperparam': {'lr': 0.01}},
+            {'key': 'iteration', 'value': 10}
+        )
+        commands.append(command3)
+        CommandItem.dump_commands(commands, out_path)
+
+        # call 'take_sanpshot'
+        target(trainer)
+        commands = CommandItem.load_commands(out_path)
+        assert len(commands) == 3
+        res = commands[0].response
+        assert res['epoch'] == 1
+        assert res['iteration'] == 1
+        assert res['status'] == CommandItem.RESPONSE_SUCCESS
+        assert commands[1].response is None
+        assert commands[2].response is None
+
+        # call 'adjust_hyperparams'
+        trainer.updater.iteration = 10
+        target(trainer)
+        commands = CommandItem.load_commands(out_path)
+        assert len(commands) == 3
+        res = commands[2].response
+        assert res['epoch'] == 1
+        assert res['iteration'] == 10
+        assert res['status'] == CommandItem.RESPONSE_SUCCESS
+        assert res['body'] is not None
+        assert res['body']['optimizer'] == 'MomentumSGD'
+        assert res['body']['hyperparam'] == {'lr': 0.005}
+        assert commands[1].response is None
+
+        # call 'stop'
+        trainer.updater.iteration = 100
+        trainer.updater.epoch = 10
+        target(trainer)
+        commands = CommandItem.load_commands(out_path)
+        assert len(commands) == 3
+        res = commands[1].response
+        assert res['epoch'] == 10
+        assert res['iteration'] == 100
+        assert res['status'] == CommandItem.RESPONSE_SUCCESS
+        assert res['body'] is None
+        assert trainer.stop_trigger._loop_stop
+
         target.finalize()
         assert CommandsState.job_status(out_path) == JobStatus.STOPPED
+
+    def test_call_invalid_request(self):
+        out_path = os.path.join(self._dir, 'results')
+        os.makedirs(out_path)
+        commands_path = os.path.join(out_path, 'commands')
+        open(commands_path, 'w').close()
+        assert os.path.isfile(commands_path)
+
+        # initialize with non-hyperparam optimizer
+        target = CommandsExtension()
+        optimizer = MagicMock()
+        optimizer.__class__.__name__ = 'MomentumSGD'
+        updater = MagicMock()
+        updater.epoch = 1
+        updater.iteration = 1
+        updater.get_optimizer.return_value = optimizer
+        trainer = _MockTrainer(out_path, updater=updater)
+        target.initialize(trainer)
+
+        # setup invalid command
+        commands = CommandItem.load_commands(out_path)
+        command = CommandItem(name='adjust_hyperparams')
+        command.set_request(
+            CommandItem.REQUEST_OPEN,
+            {'optimizer': 'Adam', 'hyperparam': {'alpha': 1.0}},
+            {'key': 'iteration', 'value': 1}
+        )
+        commands.append(command)
+        command2 = CommandItem(name='adjust_hyperparams')
+        command2.set_request(
+            CommandItem.REQUEST_OPEN,
+            {'optimizer': 'MomentumSGD', 'hyperparam': {'lr': 0.01}},
+            {'key': 'iteration', 'value': 1}
+        )
+        commands.append(command2)
+        command3 = CommandItem(name='invalid_command')
+        command3.set_request(CommandItem.REQUEST_OPEN, None, None)
+        commands.append(command3)
+        CommandItem.dump_commands(commands, out_path)
+
+        # call invalid commands
+        target(trainer)
+        commands = CommandItem.load_commands(out_path)
+        assert len(commands) == 3
+        res = commands[0].response
+        assert res['epoch'] == 1
+        assert res['iteration'] == 1
+        assert res['status'] == CommandItem.RESPONSE_SUCCESS
+        assert res['body'] is None
+        res = commands[1].response
+        assert res['epoch'] == 1
+        assert res['iteration'] == 1
+        assert res['status'] == CommandItem.RESPONSE_SUCCESS
+        assert res['body'] is None
+        res = commands[2].response
+        assert res['epoch'] == 1
+        assert res['iteration'] == 1
+        assert res['status'] == CommandItem.RESPONSE_FAILURE
+        assert res['body'] is not None
+        assert 'not available' in res['body']['message']
