d1cde523dfcdf087d1fdfbce168196e01d5bc205
ofk <ofkjpn+github@gmail.com>
2018-04-12 17:12:02 +0900

    Remove react-hot-loader

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,30 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
 import './styles.css';
 import Root from './components/Root';
 
-
-const render = (Component, appNode) => {
-  ReactDOM.render(
-    <AppContainer>
-      <Component />
-    </AppContainer>,
-    appNode
-  );
-};
-
-if (module.hot) {
-  const appNode = document.createElement('div');
-  document.body.appendChild(appNode);
-  render(Root, appNode);
-  module.hot.accept('./containers/Root', () => { render(Root, appNode); });
-} else {
-  document.addEventListener('DOMContentLoaded', () => {
-    const appNode = document.getElementById('chainerui-root');
-    if (appNode) {
-      render(Root, appNode);
-    }
-  });
-}
-
+document.addEventListener('DOMContentLoaded', () => {
+  const appNode = document.getElementById('chainerui-root');
+  if (appNode) {
+    ReactDOM.render(<Root />, appNode);
+  }
+});
