2d13be884f50d36f5962a8f70094ad8b2e4fca94
ofk <ofkjpn+github@gmail.com>
2018-05-11 18:33:11 +0900

    Fix temporary file permission issue

--- chainerui/utils/__init__.py
+++ chainerui/utils/__init__.py
@@ -5,3 +5,4 @@ from chainerui.utils.is_jsonable import is_jsonable  # NOQA
 from chainerui.utils.is_numberable import is_numberable  # NOQA
 from chainerui.utils.log_report import LogReport  # NOQA
 from chainerui.utils.save_args import save_args  # NOQA
+from chainerui.utils.tempdir import tempdir  # NOQA
