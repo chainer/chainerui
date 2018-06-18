5cb55f2eb099b9039b43b9c990ff6dac8ba091d0
TANAKA Daisuke <duaipp@gmail.com>
2018-06-18 18:53:14 +0900

    fix tests

--- tests/extensions_tests/test_image_reporter_extension.py
+++ tests/extensions_tests/test_image_reporter_extension.py
@@ -24,14 +24,22 @@ class TestImageReport(unittest.TestCase):
         if os.path.exists(self._dir):
             shutil.rmtree(self._dir)
 
+    def _make_trainer_mock(self, epoch, iteration):
+        updater = MagicMock()
+        updater.epoch = epoch
+        updater.epoch_detail = epoch
+        updater.iteration = iteration
+        trainer = MagicMock()
+        trainer.out = self._dir
+        trainer.updater = updater
+        return trainer
+
     def _equal_image(self, img1, img2):
         from PIL import ImageChops
         return ImageChops.difference(img1, img2).getbbox() is None
 
-    def _make_image_summary_value(self, img, row=None, mode=None):
-        value = dict(array=img)
-        if row is not None:
-            value['row'] = row
+    def _make_image_info(self, img, mode=None):
+        value = {'image': img}
         if mode is not None:
             value['mode'] = mode
         return value
@@ -54,15 +62,8 @@ class TestImageReport(unittest.TestCase):
 
     @unittest.skipUnless(
         image_reporter_extension._available, 'Pillow is not installed')
-    def test_call(self):
-        from PIL import Image
-        updater = MagicMock()
-        updater.epoch = 0
-        updater.epoch_detail = 0
-        updater.iteration = 1
-        trainer = MagicMock()
-        trainer.out = self._dir
-        trainer.updater = updater
+    def test_call_empty(self):
+        trainer = self._make_trainer_mock(0, 1)
 
         target = ImageReport()
         target.initialize(trainer)
@@ -70,45 +71,47 @@ class TestImageReport(unittest.TestCase):
         assert len(target._infos) == 0
 
         # count up epoch, but no image
-        updater.epoch = 1
-        updater.epoch_detail = 1
+        trainer.updater.epoch = 1
+        trainer.updater.epoch_detail = 1
         target(trainer)
         assert len(target._infos) == 0
 
-        image_prefix = summary.CHAINERUI_IMAGE_PREFIX
-        img1 = np.zeros(3000, dtype=np.uint8).reshape((10, 10, 10, 3))
-        img1[0, 0, 0, 0] = 1
-        observation = summary.chainerui_image_observer.observation
-        observation[image_prefix+'/name1'] = self._make_image_summary_value(
-            img1, 5, 'hsv')
-        img2 = np.zeros(1000, dtype=np.uint8).reshape((10, 10, 10))
-        img2[0, 0, 1] = 1
-        observation[image_prefix+'/name2'] = self._make_image_summary_value(
-            img2, 2)
+    @unittest.skipUnless(
+        image_reporter_extension._available, 'Pillow is not installed')
+    def test_call_twice(self):
+        from PIL import Image
 
-        # add image as batch
-        updater.epoch = 2
-        updater.epoch_detail = 2
-        updater.iteration = 100
+        # register stuck image, color(hsv) and grayscale
+        image_prefix = summary.CHAINERUI_IMAGE_PREFIX
+        img1 = np.zeros(50*25*3, dtype=np.uint8).reshape((50, 25, 3))
+        img1[0, 0, 0] = 1
+        observation = summary.chainerui_image_observer.observation
+        observation[image_prefix+'/name1'] = self._make_image_info(img1, 'hsv')
+        img2 = np.zeros(50*25, dtype=np.uint8).reshape((50, 25))
+        img2[0, 1] = 1
+        observation[image_prefix+'/name2'] = self._make_image_info(img2)
+
+        # make image
+        target = ImageReport()
+        trainer = self._make_trainer_mock(2, 100)
+        target.initialize(trainer)
+
         target(trainer)
         assert len(target._infos) == 1
-        png1_name = 'iter_100_%s_0.png' % target._get_hash('name1')
+        png1_name = 'iter_100_%s.png' % target._get_hash('name1')
         png1_path = os.path.join(self._dir, png1_name)
         assert os.path.exists(png1_path)
         loaded_img1 = Image.open(png1_path)
-        expected_img1 = Image.fromarray(
-            img1.reshape((5, 2, 10, 10, 3)).transpose(0, 2, 1, 3, 4).reshape(
-                50, 20, 3), mode='HSV').convert('RGB')
+        expected_img1 = Image.fromarray(img1, mode='HSV').convert('RGB')
         assert self._equal_image(loaded_img1, expected_img1)
-        png2_name = 'iter_100_%s_0.png' % target._get_hash('name2')
+        png2_name = 'iter_100_%s.png' % target._get_hash('name2')
         png2_path = os.path.join(self._dir, png2_name)
         assert os.path.exists(png2_path)
         loaded_img2 = Image.open(png2_path)
-        expected_img2 = Image.fromarray(
-            img2.reshape((2, 5, 10, 10)).transpose(0, 2, 1, 3).reshape(
-                20, 50))
+        expected_img2 = Image.fromarray(img2)
         assert self._equal_image(loaded_img2, expected_img2)
 
+        # confirm metadata
         info_path = os.path.join(self._dir, target._info_name)
         assert os.path.exists(info_path)
         with open(info_path, 'r') as f:
@@ -119,45 +122,45 @@ class TestImageReport(unittest.TestCase):
         assert len(info[0]['images']) == 2
 
         assert 'name1' in info[0]['images']
-        assert len(info[0]['images']['name1']) == 1
-        assert info[0]['images']['name1'][0] == png1_name
+        assert info[0]['images']['name1'] == png1_name
         assert 'name2' in info[0]['images']
-        assert len(info[0]['images']['name2']) == 1
-        assert info[0]['images']['name2'][0] == 'iter_100_%s_0.png' % \
-            target._get_hash('name2')
+        assert info[0]['images']['name2'] == png2_name
 
         # count up epoch, but no new image
-        updater.epoch = 3
-        updater.epoch_detail = 3
-        updater.iteration = 150
+        trainer.updater.epoch = 3
+        trainer.updater.epoch_detail = 3
+        trainer.updater.iteration = 150
         target(trainer)
         assert len(target._infos) == 1
 
-        # add 2nd. batch image, and get latest image
-        img3 = np.full(3000, 0.1).reshape((10, 10, 10, 3))
-        img3[0, 0, 0, 2] = 0.9
-        observation[image_prefix+'/name1'] = self._make_image_summary_value(
-            img3)
-        img4 = np.zeros(100, dtype=np.uint8).reshape((1, 10, 10))
-        img4[0, 0, 1] = 1
-        observation[image_prefix+'/0'] = self._make_image_summary_value(img4)
-        updater.epoch = 4
-        updater.epoch_detail = 4
-        updater.iteration = 200
+        # register 2nd. stuck image, color(float) and grayscale
+        img3 = np.full(50*25*3, 0.1).reshape((50, 25, 3))
+        img3[0, 0, 2] = 0.9
+        observation[image_prefix+'/name1'] = self._make_image_info(img3)
+        img4 = np.zeros(100, dtype=np.uint8).reshape((10, 10))
+        img4[0, 1] = 1
+        observation[image_prefix+'/name2'] = self._make_image_info(img4)
+
+        # make image
+        trainer.updater.epoch = 4
+        trainer.updater.epoch_detail = 4
+        trainer.updater.iteration = 200
         target(trainer)
         assert len(target._infos) == 2
-        for i in range(10):
-            png3_name = 'iter_200_%s_%d.png' % (target._get_hash('name1'), i)
-            png3_path = os.path.join(self._dir, png3_name)
-            assert os.path.exists(png3_path)
-            loaded_img3 = Image.open(png3_path)
-            expected_img3 = Image.fromarray(np.asarray(np.clip(
-                img3[i] * 255, 0.0, 255.0), dtype=np.uint8))
-            assert self._equal_image(loaded_img3, expected_img3)
-        png4_name = 'iter_200_%s_0.png' % target._get_hash('0')
+        png3_name = 'iter_200_%s.png' % target._get_hash('name1')
+        png3_path = os.path.join(self._dir, png3_name)
+        assert os.path.exists(png3_path)
+        loaded_img3 = Image.open(png3_path)
+        expected_img3 = Image.fromarray(target._normalize_8bit(img3))
+        assert self._equal_image(loaded_img3, expected_img3)
+        png4_name = 'iter_200_%s.png' % target._get_hash('name2')
         png4_path = os.path.join(self._dir, png4_name)
         assert os.path.exists(png4_path)
+        loaded_img4 = Image.open(png4_path)
+        expected_img4 = Image.fromarray(img2)
+        assert self._equal_image(loaded_img4, expected_img4)
 
+        # confirm metadata
         with open(info_path, 'r') as f:
             info = json.load(f)
         assert len(info) == 2
@@ -166,45 +169,34 @@ class TestImageReport(unittest.TestCase):
         assert len(info[1]['images']) == 2
 
         assert 'name1' in info[1]['images']
-        assert len(info[1]['images']['name1']) == 10
-        assert info[1]['images']['name1'][0] == 'iter_200_%s_0.png' % \
-            target._get_hash('name1')
-        assert '0' in info[1]['images']
-        assert len(info[1]['images']['0']) == 1
-        for i in range(10):
-            png3_name = 'iter_200_%s_%d.png' % (target._get_hash('name1'), i)
-            assert info[1]['images']['name1'][i] == png3_name
+        assert info[1]['images']['name1'] == png3_name
+        assert 'name2' in info[1]['images']
+        assert info[1]['images']['name2'] == png4_name
 
     @unittest.skipUnless(
         image_reporter_extension._available, 'Pillow is not installed')
     def test_with_makefn(self):
         from PIL import Image
-        updater = MagicMock()
-        updater.epoch = 1
-        updater.epoch_detail = 1
-        updater.iteration = 10
-        trainer = MagicMock()
-        trainer.out = self._dir
-        trainer.updater = updater
+        trainer = self._make_trainer_mock(1, 10)
 
         def create_image_generator(name):
             image_prefix = summary.CHAINERUI_IMAGE_PREFIX
             name = image_prefix + '/' + name
 
             def maker(trainer):
-                img = np.ones(750, dtype=np.uint8).reshape((10, 5, 5, 3))
+                img = np.ones(10*25*3, dtype=np.uint8).reshape((10, 25, 3))
+                # register image directory because to omit setup reporter
                 summary.chainerui_image_observer.observation[
-                    name] = self._make_image_summary_value(img)
+                    name] = self._make_image_info(img)
             return maker
         target = ImageReport(image_generator=create_image_generator('test'))
         target.initialize(trainer)
         target(trainer)
         assert len(target._infos) == 1
-        png_name = 'iter_10_%s_0.png' % target._get_hash('test')
+        png_name = 'iter_10_%s.png' % target._get_hash('test')
         png_path = os.path.join(self._dir, png_name)
         assert os.path.exists(png_path)
         loaded_img = Image.open(png_path)
         expected_img = Image.fromarray(
-            np.ones(750, dtype=np.uint8).reshape((5, 2, 5, 5, 3)).transpose(
-                0, 2, 1, 3, 4).reshape(25, 10, 3))
+            np.ones(10*25*3, dtype=np.uint8).reshape((10, 25, 3)))
         assert self._equal_image(loaded_img, expected_img)
--- tests/tasks_tests/test_collect_images.py
+++ tests/tasks_tests/test_collect_images.py
@@ -1,11 +1,10 @@
 import json
-from mock import MagicMock
-from mock import patch
 import os
 import shutil
 import tempfile
 import unittest
 
+from chainerui.models.result import Result
 from chainerui.tasks import collect_images
 
 
@@ -18,26 +17,17 @@ class TestApp(unittest.TestCase):
         if os.path.exists(self._dir):
             shutil.rmtree(self._dir)
 
+    def test_collect_images_no_meta(self):
+        result = Result(path_name=self._dir)
+
+        actual_list = collect_images.collect_images(result)
+        assert len(actual_list) == 0
+
     def test_collect_images(self):
-        result = MagicMock()
-        result.path_name = self._dir
-
-        mock_filter_by = MagicMock()
-        mock_filter_by.first.return_value = result
-        mock_query = MagicMock()
-        mock_query.filter_by.return_value = mock_filter_by
-        mock_session = MagicMock()
-        mock_session.query.return_value = mock_query
-
-        with patch('chainerui.tasks.collect_images.DB_SESSION', mock_session):
-            collect_images.collect_images(0)
-
         img1_1_path = os.path.join(self._dir, 'img1_1.png')
         open(img1_1_path, 'w').close()
         img1_2_path = os.path.join(self._dir, 'img1_2.png')
         open(img1_2_path, 'w').close()
-        img1_3_path = os.path.join(self._dir, 'img1_3.png')
-        open(img1_3_path, 'w').close()
         img2_path = os.path.join(self._dir, 'img2.png')
         open(img2_path, 'w').close()
 
@@ -46,8 +36,8 @@ class TestApp(unittest.TestCase):
                 'iteration': 1000,
                 'epoch': 1,
                 'images': {
-                    '0': [img1_1_path],
-                    '1': [img1_2_path, img1_3_path],
+                    '0': img1_1_path,
+                    '1': img1_2_path,
                 }
             },
             {
@@ -55,32 +45,27 @@ class TestApp(unittest.TestCase):
                 'epoch': 2,
                 'custom': 'test',
                 'images': {
-                    'seg': [img2_path],
+                    'seg': img2_path,
                 }
             }
         ]
         with open(os.path.join(self._dir, '.chainerui_images'), 'w') as f:
             json.dump(test_data, f)
 
-        with patch('chainerui.tasks.collect_images.DB_SESSION', mock_session):
-            actual_list = collect_images.collect_images(0)
+        result = Result(path_name=self._dir)
+        actual_list = collect_images.collect_images(result)
 
         assert len(actual_list) == 2
         actual_img1 = actual_list[0]
         assert len(actual_img1['contents']) == 2
         assert '0' in actual_img1['contents']
-        assert len(actual_img1['contents']['0']) == 1
         img1_1_str = collect_images._make_src_tag(
             collect_images._serialize(img1_1_path))
-        assert actual_img1['contents']['0'][0] == img1_1_str
+        assert actual_img1['contents']['0'] == img1_1_str
         assert '1' in actual_img1['contents']
-        assert len(actual_img1['contents']['1']) == 2
         img1_2_0_str = collect_images._make_src_tag(
             collect_images._serialize(img1_2_path))
-        assert actual_img1['contents']['1'][0] == img1_2_0_str
-        img1_2_1_str = collect_images._make_src_tag(
-            collect_images._serialize(img1_3_path))
-        assert actual_img1['contents']['1'][1] == img1_2_1_str
+        assert actual_img1['contents']['1'] == img1_2_0_str
         assert 'iteration' in actual_img1['train_info']
         assert actual_img1['train_info']['iteration'] == 1000
         assert 'epoch' in actual_img1['train_info']
@@ -90,3 +75,5 @@ class TestApp(unittest.TestCase):
         assert 'images' not in actual_img2['train_info']
         assert 'custom' in actual_img2['train_info']
         assert actual_img2['train_info']['custom'] == 'test'
+        assert len(actual_img2['contents']) == 1
+        assert 'seg' in actual_img2['contents']
