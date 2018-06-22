206445afc6168fcf0dcbae66a072c2a4c8991c85
TANAKA Daisuke <duaipp@gmail.com>
2018-06-22 12:28:58 +0900

    modify to use DB cache

--- chainerui/tasks/collect_images.py
+++ chainerui/tasks/collect_images.py
@@ -1,22 +1,16 @@
-from base64 import b64encode
+import datetime
 import json
 import os
 
-
-def _serialize(path):
-    with open(path, 'rb') as f:
-        data = f.read()
-    return b64encode(data).decode('utf-8')
+from chainerui import DB_SESSION
+from chainerui.models.data_info import DataInfo
+from chainerui.models.data_content import DataContent
 
 
-def _make_src_tag(src):
-    return 'data:image/png;base64,' + src
-
-
-def collect_images(result):
+def collect_images(result, data_infos, force=False):
     """collect images from meta file
 
-    Example of returning structure is:
+    Example of returning structure will be:
       [
         {
           "contents": {
@@ -35,20 +29,32 @@ def collect_images(result):
     """
     path_name = result.path_name
     info_path = os.path.join(path_name, '.chainerui_images')
+    start_idx = len(data_infos)
     if not os.path.isfile(info_path):
-        return []
+        return data_infos
+    file_modified_at = datetime.datetime.fromtimestamp(os.path.getmtime(
+        info_path))
+    if start_idx > 0:
+        if data_infos[-1].file_modified_at == file_modified_at:
+            return data_infos
+
     with open(info_path, 'r') as f:
         info_list = json.load(f)
 
-    images_list = []
-    for base_info in info_list:
-        image_path = base_info.pop('images')
-        images = {'contents': {}, 'train_info': {}}
-        for key, path in image_path.items():
-            img_str = _make_src_tag(_serialize(os.path.join(path_name, path)))
-            images['contents'][key] = img_str
-        for k, v in base_info.items():
-            images['train_info'][k] = v
-        images_list.append(images)
+    if len(info_list) < start_idx:
+        start_idx = 0
+        data_infos = []
 
-    return images_list
+    for base_info in info_list[start_idx:]:
+        image_path = base_info.pop('images')
+        data_info = DataInfo.create(result.id, base_info, file_modified_at)
+        for key, path in image_path.items():
+            with open(os.path.join(path_name, path), 'rb') as f:
+                data = f.read()
+            content = DataContent(data_info.id, path, key, data)
+            data_info.content_list.append(content)
+        data_infos.append(data_info)
+
+    DB_SESSION.commit()
+
+    return data_infos
