4ddb97d2b0dfc3bd193ec33f7f15d42048220527
Daisuke Tanaka <duaipp@gmail.com>
2018-09-12 11:29:12 +0900

    add dockerfile for deploying

--- .travis/docker_push.sh
+++ .travis/docker_push.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "$TRAVIS_TAG"
+python setup.py sdist
 echo "$DOCKERHUB_MAINTAINER_PASS" | docker login -u "$DOCKERHUB_MAINTAINER_NAME" --password-stdin
-docker build -t chainer/chainerui:"$TRAVIS_TAG" -f docker/Dockerfile .
+docker build -t chainer/chainerui:"$TRAVIS_TAG" -f docker/dockerhub/Dockerfile --build-arg CHAINERUI_VERSION="${TRAVIS_TAG:1}" .
 docker push chainer/chainerui:"$TRAVIS_TAG"
