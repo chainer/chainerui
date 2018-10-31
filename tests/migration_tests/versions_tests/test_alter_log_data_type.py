1ba6fd8c165ab9c457601fbfa629f73da2b92f56
Daisuke Tanaka <duaipp@gmail.com>
2018-10-31 17:08:09 +0900

    replace encoding arg to raw, follow latest msgpack module

--- tests/migration_tests/versions_tests/test_alter_log_data_type.py
+++ tests/migration_tests/versions_tests/test_alter_log_data_type.py
@@ -63,7 +63,7 @@ class TestUpgrade(unittest.TestCase):
             assert len(rows) == 2
             log1 = rows[0]
             assert len(log1) == 3
-            data1 = msgpack.unpackb(log1[2], encoding='utf-8')
+            data1 = msgpack.unpackb(log1[2], raw=False)
             assert len(data1) == 3
             assert data1['epoch'] == 1
             assert data1['iterations'] == 1
