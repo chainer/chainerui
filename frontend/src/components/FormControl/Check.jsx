1f0744e163b24c935686f1e9f23766daa611b2da
ofk <ofkjpn+github@gmail.com>
2018-11-16 14:07:29 +0900

    Add FormControl/Check

---
+++ frontend/src/components/FormControl/Check.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FormGroup, Label, Input } from 'reactstrap';
+
+const Check = ({ children, ...props }) => (
+  <FormGroup check>
+    <Label check className="form-check-label-break-word">
+      <Input {...props} /> {children}
+    </Label>
+  </FormGroup>
+);
+
+Check.propTypes = {
+  children: PropTypes.string
+};
+
+Check.defaultProps = {
+  children: '\u00A0'
+};
+
+export default Check;
