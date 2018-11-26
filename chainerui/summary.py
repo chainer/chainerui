16a0fd0e7d2f2dac3c9fb255ac12e665bc648d48
Daisuke Tanaka <duaipp@gmail.com>
2018-11-26 11:53:01 +0900

    fix out argument as optional

--- chainerui/summary.py
+++ chainerui/summary.py
@@ -3,6 +3,7 @@ import datetime
 import json
 import os
 import shutil
+import warnings
 
 from chainerui.utils import tempdir
 
@@ -12,9 +13,22 @@ class _Summary(object):
     def __init__(self):
         self.cache = []
         self.filename = '.chainerui_assets'
+        self.default_output_path = ''
+        self.out = None
 
     def add(self, value):
         self.cache.append(value)
+
+    def get_outpath(self, out):
+        # return output path led by argument > global priority
+        if out is not None:
+            return out
+
+        if self.out is None:
+            warnings.warn('Output directory is not set, so empty path(\'\') '
+                          'is set as output directory')
+            return self.default_output_path
+        return self.out
 
     def save(self, out):
         with tempdir(prefix='chainerui_', dir=out) as tempd:
@@ -30,9 +44,9 @@ _chainerui_asset_observer = _Summary()
 
 class _Reporter(object):
 
-    def __init__(self, out, prefix=None, **kwargs):
-        self.out = out
+    def __init__(self, prefix=None, out=None, **kwargs):
         self.prefix = prefix
+        self.out = _chainerui_asset_observer.get_outpath(out)
         self.value = kwargs
 
         self.images = {}
@@ -68,8 +82,12 @@ class _Reporter(object):
         _chainerui_asset_observer.save(self.out)
 
 
+def set_out(path):
+    _chainerui_asset_observer.out = path
+
+
 @contextlib.contextmanager
-def reporter(out, prefix=None, **kwargs):
+def reporter(prefix=None, out=None, **kwargs):
     """Summary media assets to visualize.
 
     ``reporter`` function collects media assets by the ``with`` statement and
@@ -81,27 +99,27 @@ def reporter(out, prefix=None, **kwargs):
     Example of how to set several assets::
 
        >>> from chainerui.summary import reporter
-       >>> out = '/path/to/output'
+       >>> summary.set_out('/path/to/output')  # same as 'log' file directory
        >>>
-       >>> with reporter(out, epoch=1, iteration=10) as r:
+       >>> with reporter(epoch=1, iteration=10) as r:
        >>>     r.image(image_array1)
        >>>     r.image(image_array2)
        >>> # image_array1 and image_array2 will be shown in a row.
 
     Arguments:
-        out (str): name of output path.
         prefix (str): prefix of column name.
+        out (str): directory path of output.
         **kwargs (dict): key-value pair to show as description. regardless of
             empty or not, timestamp is added.
     """
 
-    report = _Reporter(out, prefix, **kwargs)
+    report = _Reporter(prefix, out, **kwargs)
     yield report
     report.save()
 
 
-def image(images, out, name=None, ch_axis=1, row=0, mode=None, batched=True,
-          **kwargs):
+def image(images, name=None, ch_axis=1, row=0, mode=None, batched=True,
+          out=None, **kwargs):
     """Summary images to visualize.
 
     Array of images are converted as image format (PNG format on default),
@@ -113,41 +131,41 @@ def image(images, out, name=None, ch_axis=1, row=0, mode=None, batched=True,
     Examples of how to set arguments::
 
        >>> from chainerui import summary
-       >>> out = '/path/to/output'
+       >>> summary.set_out('/path/to/log')  # same as 'log' file directory
        >>>
        >>> x.shape  # = [Batchsize, Channel, Hight, Width]
        (10, 3, 5, 5)
-       >>> summary.image(x, out, name='test')  # images are tiled as 1x10
-       >>> summary.image(x, out, name='test', row=5)  # images are tiled as 2x5
+       >>> summary.image(x, name='test')  # images are tiled as 1x10
+       >>> summary.image(x, name='test', row=5)  # images are tiled as 2x5
        >>>
        >>> x.shape  # = [C, H, W]
        (3, 5, 5)
        >>> # need to set as a non-batched image and channel axis explicitly
-       >>> summary.image(x, out, name='test', ch_axis=0, batched=False)
+       >>> summary.image(x, name='test', ch_axis=0, batched=False)
        >>>
        >>> x.shape  # = [B, H, W, C]
        (10, 5, 5, 3)
        >>> # need to set channel axis explicitly
-       >>> summary.image(x, out, name='test', ch_axis=-1, row=5)
+       >>> summary.image(x, name='test', ch_axis=-1, row=5)
        >>>
        >>> x.shape  # = [H, W, C]
        (5, 5, 3)
        >>> # need to set as a non-batched image
-       >>> summary.image(x, out, name='test', ch_axis=-1, batched=False)
+       >>> summary.image(x, name='test', ch_axis=-1, batched=False)
        >>>
        >>> x.shape  # = [B, H, W], grayscale images
        (10, 5, 5)
-       >>> summary.image(x, out, name='test')  # image are tiled as 1x10
-       >>> summary.image(x, out, name='test', row=5)  # image are tiled as 2x5
+       >>> summary.image(x, name='test')  # image are tiled as 1x10
+       >>> summary.image(x, name='test', row=5)  # image are tiled as 2x5
        >>>
        >>> x.shape  # = [H, W], a grayscale image
        (5, 5)
        >>> # need to set as a non-bathed image
-       >>> summary.image(x, out, name='test', batched=False)
+       >>> summary.image(x, name='test', batched=False)
 
     Add description about the image::
 
-       >>> summary.image(x, out, name='test', epoch=1, iteration=100)
+       >>> summary.image(x, name='test', epoch=1, iteration=100)
        >>> # 'epoch' and 'iteration' column will be shown.
 
     Args:
@@ -155,7 +173,6 @@ def image(images, out, name=None, ch_axis=1, row=0, mode=None, batched=True,
             `chainer.Variable`): batch of images. If Number of dimension is
             3 (or 2 when set `batched=False`), the pixels assume as
             black and white image.
-        out (str): name of output path.
         name (str): name of image. set as column name. when not setting,
             assigned ``'image'``.
         ch_axis (int): index number of channel dimension. set 1 by default.
@@ -166,6 +183,7 @@ def image(images, out, name=None, ch_axis=1, row=0, mode=None, batched=True,
         mode (str): if the images are not RGB or RGBA space, set their
             color space code. ChainerUI supports 'HSV'.
         batched (bool): if the image is not batched, set ``False``.
+        out (str): directory path of output.
         **kwargs (dict): key-value pair to show as description. regardless of
             empty or not, timestamp on created the image is added.
     """
@@ -175,14 +193,15 @@ def image(images, out, name=None, ch_axis=1, row=0, mode=None, batched=True,
         return
     from chainerui.report.image_report import report as _image
 
+    out_path = _chainerui_asset_observer.get_outpath(out)
     col_name = name
     if col_name is None:
         col_name = 'image'
     filename, created_at = _image(
-        images, out, col_name, ch_axis, row, mode, batched)
+        images, out_path, col_name, ch_axis, row, mode, batched)
 
     value = kwargs
     value['timestamp'] = created_at.isoformat()
     value['images'] = {col_name: filename}
     _chainerui_asset_observer.add(value)
-    _chainerui_asset_observer.save(out)
+    _chainerui_asset_observer.save(out_path)
--- tests/test_summary.py
+++ tests/test_summary.py
@@ -3,6 +3,7 @@ from mock import MagicMock
 from mock import patch
 import os
 import unittest
+import warnings
 
 import numpy as np
 import pytest
@@ -14,13 +15,48 @@ from chainerui import summary
 @pytest.fixture(autouse=True, scope='function')
 def clear_cache():
     yield
+    summary._chainerui_asset_observer.out = None
     summary._chainerui_asset_observer.cache = []
+
+
+def test_summary_set_out_with_warning_image(func_dir):
+    summary._chainerui_asset_observer.default_output_path = func_dir
+    meta_filepath = os.path.join(func_dir, '.chainerui_assets')
+
+    with warnings.catch_warnings(record=True) as w:
+        warnings.simplefilter('always')
+        img = np.zeros(10*3*5*5, dtype=np.float32).reshape((10, 3, 5, 5))
+        summary.image(img)
+        assert len(w) == 1
+        assert os.path.exists(meta_filepath)
+
+        summary.set_out(func_dir)
+        summary.image(img)
+        assert len(w) == 1
+
+
+def test_summary_set_out_reporter_image(func_dir):
+    summary._chainerui_asset_observer.default_output_path = func_dir
+    meta_filepath = os.path.join(func_dir, '.chainerui_assets')
+
+    with warnings.catch_warnings(record=True) as w:
+        warnings.simplefilter('always')
+        img = np.zeros(10*3*5*5, dtype=np.float32).reshape((10, 3, 5, 5))
+        with summary.reporter() as r:
+            r.image(img)
+        assert len(w) == 1
+        assert os.path.exists(meta_filepath)
+
+        summary.set_out(func_dir)
+        with summary.reporter() as r:
+            r.image(img)
+        assert len(w) == 1
 
 
 @unittest.skipUnless(image_report._available, 'Pillow is not installed')
 def test_summary_image(func_dir):
     img = np.zeros(10*3*5*5, dtype=np.float32).reshape((10, 3, 5, 5))
-    summary.image(img, func_dir, epoch=10)
+    summary.image(img, out=func_dir, epoch=10)
 
     meta_filepath = os.path.join(func_dir, '.chainerui_assets')
     assert os.path.exists(meta_filepath)
@@ -38,7 +74,7 @@ def test_summary_image(func_dir):
     assert saved_filename.endswith('.png')
 
     img2 = np.zeros(10*3*5*5, dtype=np.float32).reshape((10, 3, 5, 5))
-    summary.image(img2, func_dir, 'test', epoch=20)
+    summary.image(img2, 'test', out=func_dir, epoch=20)
 
     with open(meta_filepath, 'r') as f:
         metas2 = json.load(f)
@@ -57,7 +93,7 @@ def test_summary_image(func_dir):
 def test_summary_image_unavailable(func_dir):
     mock_checker = MagicMock(return_value=False)
     with patch('chainerui.report.image_report.check_available', mock_checker):
-        summary.image(np.zeros(10), func_dir)
+        summary.image(np.zeros(10), out=func_dir)
 
     assert not os.path.exists(os.path.join(func_dir, '.chainerui_assets'))
 
@@ -67,7 +103,7 @@ def test_reporter(func_dir):
     img = np.zeros(10*3*5*5, dtype=np.float32).reshape((10, 3, 5, 5))
     img2 = np.copy(img)
 
-    with summary.reporter(func_dir, prefix='with_', epoch=10) as r:
+    with summary.reporter(prefix='with_', out=func_dir, epoch=10) as r:
         r.image(img)
         r.image(img2, 'test')
 
@@ -93,7 +129,7 @@ def test_reporter(func_dir):
     img3 = np.zeros(10*3*5*5, dtype=np.float32).reshape((10, 3, 5, 5))
     img4 = np.copy(img3)
 
-    with summary.reporter(func_dir, prefix='with_', epoch=20) as r:
+    with summary.reporter(prefix='with_', out=func_dir, epoch=20) as r:
         r.image(img3)
         r.image(img4, 'test')
 
@@ -116,7 +152,7 @@ def test_reporter(func_dir):
 
 @unittest.skipUnless(image_report._available, 'Pillow is not installed')
 def test_reporter_empty(func_dir):
-    with summary.reporter(func_dir, epoch=10):
+    with summary.reporter(out=func_dir, epoch=10):
         pass
 
     meta_filepath = os.path.join(func_dir, '.chainerui_assets')
@@ -126,7 +162,7 @@ def test_reporter_empty(func_dir):
 def test_reporter_image_unavailable(func_dir):
     mock_checker = MagicMock(return_value=False)
     with patch('chainerui.report.image_report.check_available', mock_checker):
-        with summary.reporter(func_dir) as r:
+        with summary.reporter(out=func_dir) as r:
             r.image(np.zeros(10))
 
     assert not os.path.exists(os.path.join(func_dir, '.chainerui_assets'))
