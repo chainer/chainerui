29183a49a8eeb01beeca8b85a5521bfe284a56d6
Daisuke Tanaka <duaipp@gmail.com>
2018-12-14 11:21:53 +0900

    add asset link button

--- frontend/src/containers/ResultDetail.jsx
+++ frontend/src/containers/ResultDetail.jsx
@@ -19,6 +19,7 @@ import ResultSummary from '../components/result/ResultSummary';
 import Args from '../components/result/Args';
 import Commands from '../components/result/Commands';
 import Snapshots from '../components/result/Snapshots';
+import Assets from '../components/result/Assets';
 import { startPolling, stopPolling } from '../utils';
 
 class ResultDetail extends React.Component {
@@ -92,6 +93,12 @@ class ResultDetail extends React.Component {
             </div>
             <div className="col-sm-6 p-2">
               <Snapshots snapshots={result.snapshots || []} />
+            </div>
+            <div className="col-sm-6 p-2">
+              <Assets
+                projectId={projectId}
+                resultId={result.id}
+              />
             </div>
           </div>
         </Container>
