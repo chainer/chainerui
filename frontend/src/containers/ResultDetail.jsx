a10e28d2107a6ebaee1f87c9573e7c7ade1246d4
Daisuke Tanaka <duaipp@gmail.com>
2018-12-14 12:08:12 +0900

    replace asset button from card style

--- frontend/src/containers/ResultDetail.jsx
+++ frontend/src/containers/ResultDetail.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Container } from 'reactstrap';
+import { Link } from 'react-router';
+import { Container, Button } from 'reactstrap';
 
 import * as uiPropTypes from '../store/uiPropTypes';
 import {
@@ -19,8 +20,7 @@ import ResultSummary from '../components/result/ResultSummary';
 import Args from '../components/result/Args';
 import Commands from '../components/result/Commands';
 import Snapshots from '../components/result/Snapshots';
-import Assets from '../components/result/Assets';
-import { startPolling, stopPolling } from '../utils';
+import { startPolling, stopPolling, urlForResultDetail } from '../utils';
 
 class ResultDetail extends React.Component {
   componentDidMount() {
@@ -72,6 +72,13 @@ class ResultDetail extends React.Component {
             project={project}
             result={result}
           />
+          <Button
+            tag={Link}
+            to={`${urlForResultDetail(projectId, result.id)}/assets`}
+            color="primary"
+          >
+            Assets
+          </Button>
           <div className="row">
             <div className="col-sm-6 p-2">
               <ResultSummary result={result} />
@@ -92,15 +99,7 @@ class ResultDetail extends React.Component {
               }
             </div>
             <div className="col-sm-6 p-2">
-              <div className="mb-2">
-                <Snapshots snapshots={result.snapshots || []} />
-              </div>
-              <div className="mb-2">
-                <Assets
-                  projectId={projectId}
-                  resultId={result.id}
-                />
-              </div>
+              <Snapshots snapshots={result.snapshots || []} />
             </div>
           </div>
         </Container>
