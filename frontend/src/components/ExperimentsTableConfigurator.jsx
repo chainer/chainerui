53f729e28138367da194f29ba6cb864114764a07
Masaki KOBAYASHI <makky.4d6b.3f5@gmail.com>
2019-03-11 21:53:25 +0900

    remove an interface to manage column visibility on ExperimentsTableConfigurator

--- frontend/src/components/ExperimentsTableConfigurator.jsx
+++ frontend/src/components/ExperimentsTableConfigurator.jsx
@@ -1,65 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {
-  Button,
-  Modal,
-  ModalHeader,
-  ModalBody,
-  ModalFooter,
-  Form,
-} from 'reactstrap';
 
 import * as uiPropTypes from '../store/uiPropTypes';
 import Check from './FormControl/Check';
 
-
 class ExperimentsTableConfigurator extends React.Component {
   constructor(props) {
     super(props);
-    this.handleModalShow = this.handleModalShow.bind(this);
-    this.handleModalHide = this.handleModalHide.bind(this);
-    this.handleChange = this.handleChange.bind(this);
     this.handleIsGrouped = this.handleIsGrouped.bind(this);
-
-    this.state = {
-      showModal: false,
-    };
-  }
-
-  handleModalShow() {
-    this.setState({
-      showModal: true,
-    });
-  }
-
-  handleModalHide() {
-    this.setState({
-      showModal: false,
-    });
-  }
-
-  handleChange(prefix, event) {
-    const { projectConfig } = this.props;
-    const { tableState } = projectConfig;
-    const {
-      hiddenLogKeys = [],
-      hiddenArgKeys = [],
-      isGrouped = false,
-    } = tableState;
-
-    if (prefix === 'logKey') {
-      const nextHiddenLogKeys = !event.target.checked
-        ? hiddenLogKeys.concat(event.target.name)
-        : hiddenLogKeys.filter((vk) => vk !== event.target.name);
-
-      this.props.onTableColumnsVisibilityUpdate(this.props.project.id, nextHiddenLogKeys, hiddenArgKeys, isGrouped);
-    } else {
-      const nextHiddenArgKeys = !event.target.checked
-        ? hiddenArgKeys.concat(event.target.name)
-        : hiddenArgKeys.filter((vk) => vk !== event.target.name);
-
-      this.props.onTableColumnsVisibilityUpdate(this.props.project.id, hiddenLogKeys, nextHiddenArgKeys, isGrouped);
-    }
   }
 
   handleIsGrouped(event) {
@@ -72,21 +20,15 @@ class ExperimentsTableConfigurator extends React.Component {
   }
 
   render() {
-    const { stats, projectConfig } = this.props;
-    const { logKeys, argKeys } = stats;
+    const { projectConfig } = this.props;
     const { tableState } = projectConfig;
     const {
-      hiddenLogKeys = [],
-      hiddenArgKeys = [],
       isGrouped = false,
     } = tableState;
 
     return (
       <div>
         <div className="form-inline">
-          <Button color="secondary" className="my-2" onClick={this.handleModalShow}>
-            Table settings
-          </Button>
           <Check
             type="checkbox"
             className="ml-2"
@@ -96,48 +38,6 @@ class ExperimentsTableConfigurator extends React.Component {
             Grouping
           </Check>
         </div>
-
-        <Modal isOpen={this.state.showModal} toggle={this.handleModalHide}>
-          <ModalHeader>
-            Edit table columns visibility
-          </ModalHeader>
-          <ModalBody>
-            <Form>
-              <legend>Log Keys</legend>
-              {
-                logKeys.map((l) => (
-                  <Check
-                    key={`logKey.${l}`}
-                    type="checkbox"
-                    name={l}
-                    checked={!hiddenLogKeys.find((vk) => vk === l)}
-                    onChange={(e) => this.handleChange('logKey', e)}
-                  >
-                    {l}
-                  </Check>
-                ))
-              }
-              <hr />
-              <legend>Arg Keys</legend>
-              {
-                argKeys.map((a) => (
-                  <Check
-                    key={`argKey.${a}`}
-                    type="checkbox"
-                    name={a}
-                    checked={!hiddenArgKeys.find((va) => va === a)}
-                    onChange={(e) => this.handleChange('argKey', e)}
-                  >
-                    {a}
-                  </Check>
-                ))
-              }
-            </Form>
-          </ModalBody>
-          <ModalFooter>
-            <Button color="secondary" onClick={this.handleModalHide}>Close</Button>
-          </ModalFooter>
-        </Modal>
       </div>
     );
   }
@@ -146,7 +46,6 @@ class ExperimentsTableConfigurator extends React.Component {
 ExperimentsTableConfigurator.propTypes = {
   project: uiPropTypes.project.isRequired,
   projectConfig: uiPropTypes.projectConfig.isRequired,
-  stats: uiPropTypes.stats.isRequired,
   onTableColumnsVisibilityUpdate: PropTypes.func.isRequired,
 };
 
