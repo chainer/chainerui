ceb6bfcf33c2ade0f0978fe07b1eb549a870be19
Daisuke Tanaka <duaipp@gmail.com>
2019-04-16 18:58:28 +0900

    support plain/text content-type

--- chainerui/client/helper.py
+++ chainerui/client/helper.py
@@ -2,42 +2,51 @@ import json
 import urllib3
 
 
-def urlopen(method, url, headers=None, data=None):
+def urlopen(method, url, content_type='json', data=None, return_type='json'):
     """connect and get information via HTTP request
 
-    A simplified wrapper for HTTP request, fixed by JSON data format.
+    A simplified wrapper for HTTP request, favor for JSON data format.
     When an error is occurred, error message is converted to text and return
-    ``(None, text)`` tuple. This simplification is favor for especially during
+    ``(None, text)`` tuple. This simplification is prefer especially to during
     training phase not to stop processes.
 
     Arguments:
         method (str): method type, GET/POST/PUT/DELETE
         url (str): target URL
-        headers (str): content type, ``'application/json'`` is set on default.
-            currently only supports JSON type.
-        data (dict or list): dict or list data, dumped as JSON automatically
+        content_type (str): content type set on HTTP request header,
+            ``'application/json'`` is set on default.
+        data (obj): when ``content_type`` is ``'json'``, request data is dumped
+            as JSON automatically.
+        return_type (str): type name. returned value is converted JSON on
+            default.
 
     Returns:
         tuple: ``(Response, '')`` when not error, ``(None, str)`` when error
             has occurred.
     """
 
-    if headers is None:
-        headers = {'Content-Type': 'application/json'}
-    json_data = None
-    if data is not None:
-        json_data = json.dumps(data)
+    headers = {}
+    body = data
+    if content_type == 'json':
+        headers['Content-Type'] = 'application/json'
+        if data is not None:
+            body = json.dumps(data)
+    else:
+        headers['Content-Type'] = content_type
     http = urllib3.PoolManager()
     try:
-        res = http.request(method, url, body=json_data, headers=headers)
+        res = http.request(method, url, body=body, headers=headers)
     except (urllib3.exceptions.NewConnectionError,
             urllib3.exceptions.MaxRetryError) as e:
-        return None, 'cannot connect to the URL: ' + url
+        return None, 'cannot connect to the URL: {}, {}'.format(url, e)
     except urllib3.exceptions.HTTPError as e:
         return None, e.message
 
-    obj = json.loads(res.data.decode('utf8'))
-    if res.status != 200:
-        return None, 'error: ' + obj.get('message', '')
+    if return_type == 'json':
+        obj = json.loads(res.data.decode('utf8'))
+        if res.status != 200:
+            return None, 'error: ' + obj.get('message', '')
+    else:
+        obj = res.data.decode('utf8')
 
     return obj, ''
