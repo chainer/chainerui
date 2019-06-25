b30f41016d25396c38b06911c02d93a870e317f8
himkt <himkt@klis.tsukuba.ac.jp>
2019-06-25 22:09:58 +0900

    Accept not only json but also jsonl

--- chainerui/tasks/collect_results.py
+++ chainerui/tasks/collect_results.py
@@ -4,14 +4,15 @@ import os
 from chainerui.database import db
 from chainerui.models.result import Result
 
-def _list_result_paths(target_path, log_file_name='log.jsonl'):
+
+def _list_result_paths(target_path, log_file_name='not used anymore'):
     """list_result_paths."""
 
     result_list = []
 
     for root, _dirs, _files in os.walk(os.path.abspath(target_path)):
         for name in _files:
-            if name == log_file_name:
+            if name == 'log.jsonl' or name == 'log':
                 result_list.append(root)
 
     return result_list
@@ -20,7 +21,11 @@ def _list_result_paths(target_path, log_file_name='log.jsonl'):
 def _register_result(project_id, result_path):
     result_path = os.path.abspath(result_path)
 
-    contain_log_file = os.path.isfile(os.path.join(result_path, 'log.jsonl'))
+    jsonl_path = os.path.join(result_path, 'log.jsonl')
+    json_path = os.path.join(result_path, 'log')
+
+    contain_log_file = os.path.isfile(jsonl_path) or \
+        os.path.isfile(json_path)
 
     if not contain_log_file:
         return
