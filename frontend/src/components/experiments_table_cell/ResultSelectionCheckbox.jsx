e890d705d44eee29964890b6babba6c4671b1290
Masaki KOBAYASHI <makky.4d6b.3f5@gmail.com>
2019-08-22 01:19:12 +0900

    Added a component of a checkbox for result state

---
+++ frontend/src/components/experiments_table_cell/ResultSelectionCheckbox.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+import { Button } from 'reactstrap';
+import Check from '../FormControl/Check';
+
+const ResultSelectionCheckbox = (props) => {
+  return <Check type="checkbox" />;
+};
+
+ResultSelectionCheckbox.propTypes = {};
+
+export default ResultSelectionCheckbox;
