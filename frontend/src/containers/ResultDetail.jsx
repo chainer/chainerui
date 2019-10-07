7749ee6dd39ef51a58169fb98a432e76cae86d4e
gky360 <gky360@gmail.com>
2019-10-07 21:15:56 +0900

    use Link component from react-router-dom

--- frontend/src/containers/ResultDetail.jsx
+++ frontend/src/containers/ResultDetail.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Link } from 'react-router';
+import { Link } from 'react-router-dom';
 import { Container, Button } from 'reactstrap';
 
 import * as uiPropTypes from '../store/uiPropTypes';
