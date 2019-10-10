5649237cfb7e26cebe4814852a783809ef744ee7
gky360 <gky360@gmail.com>
2019-10-11 00:16:22 +0900

    copy attempyRequest function from redux-requests

--- frontend/src/middleware/api.js
+++ frontend/src/middleware/api.js
@@ -1,5 +1,3 @@
-import { attemptRequest } from 'redux-requests';
-
 const API_ROOT = '/api/v1/';
 
 const callApi = (endpoint, method = 'GET', body) => {
@@ -25,6 +23,35 @@ const callApi = (endpoint, method = 'GET', body) => {
 };
 
 export const CALL_API = 'Call API';
+
+/**
+ * Helper function to attempt a request and handle the response.
+ * @param  {String} url           The URL the request is for.
+ * @param  {Object} actions       Actions to dispatch depending on the outcome of the "makeRequest" Promise.
+ * @param  {Function} makeRequest Function that returns a Promise object. This function performs the actual request.
+ * @param  {Function} dispatch    Redux store dispatch function.
+ */
+export function attemptRequest(url, actions, makeRequest, dispatch) {
+  const beginAction = { ...actions.begin() };
+  beginAction.meta = beginAction.meta || {};
+  beginAction.meta.httpRequest = { url, done: false };
+  if (!dispatch(beginAction)) {
+    return; // bail out here if the middleware cancelled the dispatch
+  }
+  makeRequest()
+    .then((response) => {
+      const successAction = { ...actions.success(response) };
+      successAction.meta = successAction.meta || {};
+      successAction.meta.httpRequest = { url, done: true };
+      dispatch(successAction);
+    })
+    .catch((err) => {
+      const failureAction = { ...actions.failure(err) };
+      failureAction.meta = failureAction.meta || {};
+      failureAction.meta.httpRequest = { url, done: true };
+      dispatch(failureAction);
+    });
+}
 
 export default (store) => (next) => (action) => {
   const callAPI = action[CALL_API];
