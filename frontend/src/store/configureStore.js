e29b4b19b844730e7d2fe605eb68cf32c7c13cd6
gky360 <gky360@gmail.com>
2019-10-11 17:26:36 +0900

    copy createRequestMiddleware function from redux-requests

--- frontend/src/store/configureStore.js
+++ frontend/src/store/configureStore.js
@@ -1,9 +1,31 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import { createRequestMiddleware } from 'redux-requests';
 import { createLogger } from 'redux-logger';
 import api from '../middleware/api';
 import rootReducer from '../reducers';
+
+/**
+ * Creates a Redux middleware function when called.
+ * @param  {Function} selectorFunc A function to select the location in the store's state tree where
+ *                                   the requests reducer keeps it's state.
+ * @return {Function}              A middleware function that will only dispatch the action if the
+ *                                   action.meta.httpRequest.done property is false, and the
+ *                                   meta.httpRequest.url is not already in flight.
+ */
+export function createRequestMiddleware(selectorFunc = (state) => state.requests) {
+  return (store) => (next) => (action) => {
+    // Cancel HTTP request if there is already one pending for this URL
+    if (action.meta && action.meta.httpRequest && !action.meta.httpRequest.done) {
+      const requests = selectorFunc(store.getState());
+      if (requests[action.meta.httpRequest.url]) {
+        // There is a request for this URL in flight already!
+        // (Ignore the action)
+        return undefined;
+      }
+    }
+    return next(action);
+  };
+}
 
 const configureStore = (preloadedState) => {
   const middleware = [thunk, api, createRequestMiddleware(), createLogger()];
