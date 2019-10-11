b666359120b936213dbcb65ead730f7f08fff820
gky360 <gky360@gmail.com>
2019-10-11 17:29:14 +0900

    copy requestsReducer from redux-requests

--- frontend/src/reducers/index.jsx
+++ frontend/src/reducers/index.jsx
@@ -1,7 +1,6 @@
 import { combineReducers } from 'redux';
 import { routerReducer, LOCATION_CHANGE } from 'react-router-redux';
 import { persistReducer } from 'redux-persist';
-import { requestsReducer } from 'redux-requests';
 import storage from 'redux-persist/es/storage';
 import * as ActionTypes from '../actions';
 import {
@@ -13,6 +12,30 @@ import {
   keyOptions,
   fetchResultTypes,
 } from '../constants';
+
+/**
+ * Reducer function to handle pending requests.
+ * @param  {Object} state  Existing state object.
+ * @param  {Object} action Incoming action:
+ *                         - Actions with the meta.httpRequest property are examined.
+ *                         - The meta.httpRequest.url property is added or removed
+ *                           from the current state depending on if the meta.httpRequest.done
+ *                           property is set.
+ * @return {Object}        The new state.
+ */
+export function requestsReducer(state = {}, action) {
+  if (!action.meta || !action.meta.httpRequest || !action.meta.httpRequest.url) {
+    return state;
+  }
+  if (action.meta.httpRequest.done) {
+    // Remove this request from the state
+    const newState = { ...state };
+    delete newState[action.meta.httpRequest.url];
+    return newState;
+  }
+  // Add this request to the state
+  return { ...state, [action.meta.httpRequest.url]: true };
+}
 
 const updatePartialState = (state, action, keyId, fn) => {
   const partialState = fn(state[keyId], action);
